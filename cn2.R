c5dbbb56c2d9e3a577c8d266ea22aedc05ccf3e8
santidrj <santi.delrey@gmail.com>
2022-03-14 20:54:47 +0100

    Start implementing find.best.complex

--- cn2.R
+++ cn2.R
@@ -2,13 +2,12 @@ library(data.table)
 library(dplyr)
 library(rlang)
 
-data <- read.csv(file.path('datasets', 'breast-cancer.csv'), header = T)
+data <- read.csv(file.path('datasets', 'breast-cancer.csv'), header = T, stringsAsFactors = T)
 
 # The algorithm assumes that the class is the last column of the dataset.
 
 data <- data %>%
   rename(y = last_col())
-
 
 most.common <- function(x){
   ## Returns the most common factor in a vector.
@@ -17,17 +16,68 @@ most.common <- function(x){
   toString(dd[which.max(tabulate(match(x,dd)))])
 }
 
-find.best.complex <- function(E) {
-  # paste(colnames(E), collapse = " && ")
+get.selectors <- function(x) {
+  selectors <- list()
+  x.attr <- names(x)
+  for (i in seq(length(x.attr))) {
+    values <- x[[i]]
+    if (is.factor(values)) {
+      
+      selectors[[length(selectors)+1]] <- levels(values)
+    } else {
+      selectors[[length(selectors)+1]] <- unique(values)
+    }
+    # for (value in values) {
+    #   selectors <- append(selectors, paste(name,value, sep = " == "))
+    # }
+  }
+  names(selectors) <- x.attr
+  selectors
+}
+
+format.complex <- function(x) {
+  complex.list <- list()
+  for (name in names(x)) {
+    for (value in x[name]) {
+      complex.list <- append(complex.list, paste(name,value, sep = " == "))
+    }
+  }
+  complex.list
+}
+
+find.best.complex <- function(E, selectors) {
+  star <- list()
+  best.cpx <- NULL
+  combinations <- list()
+  repeat  {
+    if (length(star) == 0) {
+      new.star <- selectors
+      combinations <- names(selectors)
+      complex.list <- format.complex(new.star)
+    } else{
+      new.combinations <- list()
+      for (name in names(star)) {
+        new.combinations <- append(new.combinations, as.list(outer(name, names(selectors)[names(selectors) != name], paste)))
+        aux <- expand.grid(star[name], selectors[names(selectors) %in% name == FALSE])
+      }
+    }
+    
+    star <- new.star
+
+    if (length(star) <= 0) {
+      break
+    }
+  }
+  paste(colnames(E), collapse = " && ")
   "irradiant == 'no' || irradiant == 'yes'"
 }
 
-CN2 <- function(E) {
+CN2 <- function(E, selectors) {
   aux.copy <- copy(E)
   rule.list <- c()
   best.cpx <- ""
   while (!is.null(best.cpx) && dim(aux.copy)[1] != 0) {
-    best.cpx <- find.best.complex(E)
+    best.cpx <- find.best.complex(E, selectors)
     if (!is.null(best.cpx)) {
       selected <- data %>%
         filter(!! parse_expr(best.cpx))
@@ -39,5 +89,6 @@ CN2 <- function(E) {
   rule.list
 }
 
-rules <- CN2(data)
+selectors <- get.selectors(select(data, !y))
+rules <- CN2(data, selectors)
 
